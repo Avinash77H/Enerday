975133f5636384edc595f1056523937ed421fc81
Avinash <arakholiya5@gmail.com>
2026-02-11 23:20:25 +0530

    cherry pick one commit from demo-1 branch

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 export default function Home() {
   console.log("This is demo-2 page changes");
   
+  console.log("This is home page");
+  console.log("This is demo page");
+  
   return (
     <div>
       <main>
